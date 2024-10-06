29e81144d83c3a38f5f7667f5a49239444cdf141
skanderyakoubi <iskandar.yaakoubi@supcom.tn>
2024-10-06 14:23:29 +0100

    nex commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str) -> list[str] :
+
+    ''' 
+    this function will return a list of requirements 
+    '''
+    requirements=[]
+    with open(file_path) as file_obj :
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+setup(
+   name='foo',
+   version='1.0',
+   description='A useful module',
+   author='skander yaakoubi',
+   author_email='iskandar.yaakoubi@supcom.tn',
+   packages= find_packages(),  #same as name
+   install_requires=['pandas', 'numpy', 'seaborn'], #external packages as dependencies
+)
+
