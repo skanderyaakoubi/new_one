6b9578980ccbdd77c843f7ea6844280cc87a33eb
skanderyakoubi <iskandar.yaakoubi@supcom.tn>
2024-11-05 04:26:42 +0100

    train model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,13 +2,17 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-
-
 import pandas as pd
-from sklearn.model_selection import train_test_split 
+
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 class DataIngestionConfig :
     train_data_path: str=os.path.join("artifactss","train.csv")
     test_data_path: str=os.path.join("artifactss","test.csv")
@@ -24,7 +28,7 @@ class DataIngestion :
             df=pd.read_csv("notebook\data\stud.csv")  #you can change this line and read data from mongoi db , my sql
             logging.info("Read dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            #os.makedirs(os.path.dirname(self.ingestion_config.train_data_path)) 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("train test split")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
@@ -40,7 +44,16 @@ class DataIngestion :
             raise CustomException(e,sys)   
 
 
-if __name__=="__main__" :
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()  #used to execute the code
 
+        
+if __name__=="__main__":
+
+
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
